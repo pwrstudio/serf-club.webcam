4f6cab3efdda7f0884382a37b7a64cf04db9ed5f
pwrstudio <pwrpntr@gmail.com>
2017-12-30 22:38:56 +0100

    update server address

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import 'babel-polyfill'
 import App from './App'
 import VueSocketio from 'vue-socket.io'
 
-Vue.use(VueSocketio, 'http://34.203.130.113:8080')
+// Vue.use(VueSocketio, 'http://34.203.130.113:8080')
+Vue.use(VueSocketio, 'https://serfclubengine-udagtjujws.now.sh')
 // Vue.use(VueSocketio, 'http://192.168.0.65:7070')
 
 // --- ERROR TRACKING
